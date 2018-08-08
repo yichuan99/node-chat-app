eae04231340a6a94eb740ea49f03fdeeef0e6840
John Wang <wycsasa@gmail.com>
2018-08-07 21:41:22 -0400

    Setup socket.io custom event listner

--- server/server.js
+++ server/server.js
@@ -23,6 +23,12 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
 	console.log("New user connected");
 
+
+	socket.on("createMessage", (message) => {
+		console.log(message);
+		socket.emit("newMessage", message);
+	});
+
 	socket.on("disconnect", () =>{
 		console.log("Disonnected from client");
 	}); 
