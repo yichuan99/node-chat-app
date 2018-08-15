4837380a02029b62c5dc075570d77adb3eabcf9d
John Wang <wycsasa@gmail.com>
2018-08-14 21:12:45 -0400

    added form and showing incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,19 @@ socket.on("disconnect", () =>{
 
 socket.on("newMessage", function (message){
 	console.log("New Message: ", message);
+	var li = jQuery("<li></li>"); // use jQuery to create element and add it to the page
+	li.text(`${message.from}:${message.text}`);
+
+	jQuery("#messages").append(li);
+});	
+
+jQuery("#message-form").on("submit", function (e) {
+	e.preventDefault();
+
+	socket.emit("createMessage", {
+		from: "User",
+		text: jQuery("[name=message]").val()
+	}, function () {
+
+	});
 });
